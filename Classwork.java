56e96f22094be9efe7a0ea2bc19c7f4e52cb65cb
datboisus <donvu251106@gmail.com>
2024-10-02 10:20:46 -0700

    1

--- Classwork.java
+++ Classwork.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
 public class Classwork
 {
   public static void main(String[] args)
   {
-    // write your code
+    Scanner sc = new Scanner (System.in);
+    System.out.println("What type of item are you buying?");
+    String item = sc.nextLine();
+    System.out.println(item);
+    
+    Scanner sc = new Scanner (System.in);
+    System.out.println("How many are you buying?");
+    
+    
+
+
+
   }
 }
